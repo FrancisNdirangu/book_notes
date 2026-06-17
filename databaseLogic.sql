5440c0b01b8414c7ece4a42d88cc8d55903e01ad
unknown <frankngugi@gmail.com>
2026-06-17 15:44:20 +0300

    wrote the code that will be used to create the database in databaseLogic.sql

---
+++ databaseLogic.sql
@@ -0,0 +1,9 @@
+--Create a users table for the book notes
+CREATE TABLE book-notes (
+    id SERIAL PRIMARY KEY,
+    title:CHAR(100),
+    notes:CHAR(2000),
+    date_read:DATE,
+    olid:CHAR(100),
+    book_cover_link:CHAR(1000)
+);
