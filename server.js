377ff800b04a962ce7208805fd290b5ceeda9ee4
unknown <frankngugi@gmail.com>
2026-06-17 15:52:40 +0300

    set the viewing engine to ejs

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Setting the viewing engine to EJS
+app.set("view_engine", "ejs");
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
