1342715d209347fde3d80db97fca3144677f3dcd
unknown <frankngugi@gmail.com>
2026-06-19 19:06:43 +0300

    created a connection to the database in the server file and added the important details in the env file

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ app.use(express.static("public"));
 //Setting the viewing engine to EJS
 app.set("view_engine", "ejs");
 
+const db = new pg.Client({
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
+const testQuery = await db.query("SELECT * from book_notes");
+console.log(testQuery.rows);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
