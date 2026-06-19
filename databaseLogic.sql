19dae29f428ca397882a0b257d5a4617dbb19704
unknown <frankngugi@gmail.com>
2026-06-19 18:48:42 +0300

    removed the colons that arent needed in the database logic

--- databaseLogic.sql
+++ databaseLogic.sql
@@ -1,9 +1,9 @@
 --Create a users table for the book notes
 CREATE TABLE book_notes (
     id SERIAL PRIMARY KEY,
-    title:CHAR(100),
-    notes:CHAR(2000),
-    date_read:DATE,
-    olid:CHAR(100),
-    book_cover_link:CHAR(1000)
+    title CHAR(100),
+    notes CHAR(2000),
+    date_read DATE,
+    olid CHAR(100),
+    book_cover_link CHAR(1000)
 );
