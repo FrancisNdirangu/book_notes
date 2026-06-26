88d5b2c1be7282bafe631e3623335dd548f5e854
unknown <frankngugi@gmail.com>
2026-06-26 20:11:55 +0300

    created a database.js file where I will be initializing the connection to the database

---
+++ database.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+
+const db = new pg.Client({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+db.connect();
+
+db.on("error", (error) => {
+  console.error("unable to establish connection to database", error);
+});
+
+export default pool;
