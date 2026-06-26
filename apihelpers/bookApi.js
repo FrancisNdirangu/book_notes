2c6e620c0c76bbd5a6feda54f1e67e3f6b51fc12
unknown <frankngugi@gmail.com>
2026-06-26 20:43:07 +0300

    created a function in bookApi.js that will request information on the name of a book from the openlibrary search api

---
+++ apihelpers/bookApi.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import axios from "axios";
+
+export const booksJsonResponse = async (book_name) => {
+  const response = await axios.get(process.env.SEARCH_URL + book_name);
+  console.log(response);
+};
