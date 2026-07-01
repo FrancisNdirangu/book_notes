43f1c38fc703e20ba296a8c5da287965bb5a3852
unknown <frankngugi@gmail.com>
2026-07-01 10:12:14 +0300

    created a function to connect to the database

--- config/db.js
+++ config/db.js
@@ -2,5 +2,14 @@ import "dotenv/config";
 import pg from "pg";
 
 const db = new pg.Client({
-  host: proce,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
+export async function connectDatabase() {
+  await db.connect();
+  console.log("Connected to PotgreSQL");
+}
