fa31ebf162e675afbb16d0f8932ded2b3cd97a03
unknown <frankngugi@gmail.com>
2026-07-10 16:13:23 +0300

    made if statement that returns the book titles for the rows with empty titles

--- middleware/dbCheck.js
+++ middleware/dbCheck.js
@@ -1,10 +1,17 @@
 import {apiModel} from "../models/apiDataModel.js";
+import {booksJsonResponse} from "../apihelpers/bookApi.js"
 
 export const nullOlidRows = async (req,res,next) => {
   try{
 
     const nullRecords = await apiModel.checkNullOLID();
     console.log(nullRecords);
+
+    if (nullRecords) {
+      nullRecords.forEach((record) => {
+        console.log(record.title);
+      });
+    }
     next() //tells the middleware to move on to the controller
 
   } catch (error) {
