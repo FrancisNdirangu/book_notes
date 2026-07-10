b643ccde16df8372d35a71b216e86a6a6fb61499
unknown <frankngugi@gmail.com>
2026-07-10 13:03:32 +0300

    changed the bookJsonResponse api to properly format the book names so that the query gives the correct resposne

--- apihelpers/bookApi.js
+++ apihelpers/bookApi.js
@@ -2,7 +2,8 @@ import "dotenv/config";
 import axios from "axios";
 
 export const booksJsonResponse = async (book_name) => {
-  const response = await axios.get(process.env.SEARCH_URL + book_name);
+  const properly_formatted_book_name = encodeURIComponent(book_name).replace('/%20/g', '+')
+  const response = await axios.get(process.env.SEARCH_URL + properly_formatted_book_name);
   console.log(response);
   return response;
 };
