27637594021495905be53ac9bfd8301252a9d508
unknown <frankngugi@gmail.com>
2026-07-10 16:50:51 +0300

    changed the title column type to varchar and now getting the correct responses from search api. i then made the apihelper return the top result json from the search so that later we can parse it to store the olid information into the db

--- apihelpers/bookApi.js
+++ apihelpers/bookApi.js
@@ -5,8 +5,10 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const booksJsonResponse = async (book_name) => {
   const properly_formatted_book_name = encodeURIComponent(book_name).replace('/%20/g', '+')
+  console.log(properly_formatted_book_name);
   const response = await axios.get(process.env.SEARCH_URL + properly_formatted_book_name);
-  console.log(response);
+  console.log(JSON.stringify(response.data['docs'][0]));
+
   await sleep(2000); // a delay so that the api doesnt get rate limited
-  return response;
+  return response.data['docs'][0];
 };
--- middleware/dbCheck.js
+++ middleware/dbCheck.js
@@ -5,11 +5,11 @@ export const nullOlidRows = async (req,res,next) => {
   try{
 
     const nullRecords = await apiModel.checkNullOLID();
-    console.log(nullRecords);
+    //console.log(nullRecords);
 
     if (nullRecords) {
       nullRecords.forEach((record) => {
-        console.log(record.title);
+        //console.log(record.title);
         const bookInfoResponse = booksJsonResponse(record.title);
       });
     }
