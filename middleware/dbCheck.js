0fadfbbfff1ae8acfbdb912a2a046b2be422c915
unknown <frankngugi@gmail.com>
2026-07-22 18:49:08 +0300

    created a middleware called createBookCoverLinks that creates the full book cover links for the rows with null links

--- middleware/dbCheck.js
+++ middleware/dbCheck.js
@@ -20,3 +20,21 @@ export const nullOlidRows = async (req,res,next) => {
     next(error);
   }
 }
+
+export const createBookCoverLinks = async (req, res, next) => {
+  try {
+    const nullLinks = await apiModel.checkNullBookCoverLink();
+    const cover_base_link = "https://covers.openlibrary.org/b/olid/";
+    console.log(nullLinks);
+    if (nullLinks) {
+      nullLinks.forEach(async (record) => {
+        const current_olid = await record.olid
+        const link = cover_base_link + current_olid + "-M.jpg";
+        console.log(link)
+      })
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
