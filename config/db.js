4b6bbf6b6f5c1d4355c33af05c008a72f499d8a1
unknown <frankngugi@gmail.com>
2026-07-27 17:12:40 +0300

    created the db configurations for the application database and the maintenance database (the maintenance database being for checkign whether the application db exists when creating it

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 import "dotenv/config";
 import pg from "pg";
 
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
 export const db = new pg.Client({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -18,5 +26,17 @@ export async function disconnectDatabase() {
   await db.end();
   console.log("Disconnected from PostgreSQL");
 }
+
+//Create a client connected to the application's database
+export function createAppDatabaseClient() {
+  return new pg.Client(dbConfig);
+}
+
+//Creates a client connected to PostgreSQL's maintenance database.
+// This connection is for checking whether the application db exists when creating it.
+export function createAdminDatabaseClient() {
+  return new pg.Client({ ...dbConfig, database: process.env.DB_ADMIN_DATABASE ?? 'postgres' });
+
+  }
 
 export default db;
