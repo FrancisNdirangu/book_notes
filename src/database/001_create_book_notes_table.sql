b87db50ff05d264fec3f95be176506d622e569eb
unknown <frankngugi@gmail.com>
2026-07-27 17:29:19 +0300

    added the table and columns that are to be created in the migration file

---
+++ src/database/001_create_book_notes_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE book_notes (
+    id serial not null, primary key,
+    title VARCHAR(100),
+    notes VARCHAR(2000),
+    date_read date,
+    olid VARCHAR(100),
+    book_cover_link VARCHAR(1000),
+    rating INTEGER,
+    original_title VARCHAR(150),
+    publication_year VARCHAR(100),
+    AUTHOR VARCHAR(100),
+    series_name VARCHAR(200),
+    series_position VARCHAR(100),
+    );
