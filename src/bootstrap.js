08b2b106047891994e4c7ca18b67598f47f99c10
Francis Ndirangu <frankngugi@gmail.com>
2026-07-28 13:46:55 +0300

    made a function for validating the database names

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,3 +8,20 @@ const currentFilePath = fileURLToPath(import.meta.url);
 const currentDirectory = path.dirname(currentFilePath);
 
 const migrationsDirectory = path.join(currentDirectory, "migrations");
+
+/**
+ * Postgres $1 parameters cant represent identifiers like database names.
+ * So I'll validate the database name before we insert it into the create database statement */
+
+function validateDatabaseName(databaseName) {
+  const validateDatabaseName = /^[a-zA-z_][a-zA-Z0-9_]*$/;
+
+  if (!validateDatabaseName.test(databaseName)) {
+    throw new Error(
+      `Invalid database name: "${databaseName}".` +
+      "Use only letters, numbers, and underscores."
+    );
+    ;
+    
+  }
+}
