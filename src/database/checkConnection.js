f2b29c8df497e77b238f45a5e0ba573ccc7e25af
Francis Ndirangu <frankngugi@gmail.com>
2026-08-04 17:44:12 +0300

    created checkConnection.js file to make sure that we are conected to the app database

---
+++ src/database/checkConnection.js
@@ -0,0 +1,17 @@
+import { createAppDatabaseClient} from "../config/db.js"
+
+export async function verifyDatabaseConnection() {
+  const client = createAppDatabaseClient();
+
+  try{
+    await client.connect();
+
+    const result = await client.query(`SELECT current_database() AS database_name,
+                                        CURRENT_TIMESTAMP AS connected_at`);
+
+    console.log(`Connected to database ${result.rows[0].database_name}`);
+
+  } finally {
+    await client.end().catch(() => {});
+  }
+}
