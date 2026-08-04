25a78e1b52f6c14c3a5d2992c20d18d9f862a6c8
Francis Ndirangu <frankngugi@gmail.com>
2026-08-04 18:44:22 +0300

    created migration 2 which adds 3 records into the booknotes db so that we have a few titles, notes, date_read and ratings

---
+++ src/database/migrations/002_add_a_few_records_in_book_notes.sql
@@ -0,0 +1,3 @@
+INSERT INTO book_notes (title,notes,date_read,rating)
+VALUES ('Downward to the Earth','A book about spiritual transformation','2024-07-20',5),('Empire of Silence','A book about a the rise of Hadrian Marlowe','2023-10-21',5),
+('Solaris','A psychologist is sent to investigate why scientists are going crazy when they visit a certain planet','2024-01-07',5);
