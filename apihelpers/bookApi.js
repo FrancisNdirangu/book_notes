bcfff0e36aaebb62c7e0d7654c7c45ecd57faa43
Francis Ndirangu <frankngugi@gmail.com>
2026-08-05 19:57:14 +0300

    added the SEARCH_URL variable to the .env file. which is the link that forms the first part of the open library search url for the book titles

--- apihelpers/bookApi.js
+++ apihelpers/bookApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export const booksJsonResponse = async (book_name) => {
-  const properly_formatted_book_name = encodeURIComponent(book_name).replace(/%20/g, "+");
+  const properly_formatted_book_name = encodeURIComponent(book_name.trim()).replace(/%20/g, "+");
   console.log(properly_formatted_book_name);
   const response = await axios.get(process.env.SEARCH_URL + properly_formatted_book_name);
   //console.log(JSON.stringify(response.data['docs'][0]));
