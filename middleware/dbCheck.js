66c85599b5cf20f8dd51152480fac7fc9246cd35
Francis Ndirangu <frankngugi@gmail.com>
2026-08-05 20:12:33 +0300

    currently now getting the images for the olid

--- middleware/dbCheck.js
+++ middleware/dbCheck.js
@@ -24,11 +24,13 @@ export const nullOlidRows = async (req,res,next) => {
 export const createBookCoverLinks = async (req, res, next) => {
   try {
     const nullLinks = await apiModel.checkNullBookCoverLink();
+    //console.log('Null Links records:',nullLinks.rows);
     const cover_base_link = "https://covers.openlibrary.org/b/olid/";
-    console.log(nullLinks);
+    //console.log(nullLinks);
     if (nullLinks) {
-      nullLinks.forEach(async (record) => {
-        const current_olid = await record.olid
+      nullLinks.forEach((record) => {
+        const current_olid = record.olid
+        console.log('current olid:',current_olid);
         const link = cover_base_link + current_olid + "-M.jpg";
         const addLinksToDB = apiModel.addBookCoverLink(link, record.id)
       })
