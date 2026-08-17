363b9cc6cc728918283afbdc9b6be9315a1a3530
Francis Ndirangu <frankngugi@gmail.com>
2026-08-17 16:29:34 +0300

    created a function that updated the olid and bookcover

--- models/helperModel.js
+++ models/helperModel.js
@@ -8,3 +8,9 @@ export function currentTitle(id) {
   console.log(`current title is ${title}`)
   return title
 }
+
+export function updateBookCover(id,updatedOLID,book_cover_link) {
+  const query = "UPDATE book_notes SET olid = $1, book_cover_link=$2  WHERE id=$3"
+  const update = db.query(query,[updatedOLID,book_cover_link,id]);
+  console.log(`OLID and book cover link have been updated for id: ${id}`);
+}
