4b0eb91304f50a70f44772d4b854907034f77d46
Francis Ndirangu <frankngugi@gmail.com>
2026-08-17 15:46:21 +0300

    importing the currentTitle helper function in the bookService and defining the variables ill need to compare

--- services/bookService.js
+++ services/bookService.js
@@ -1,9 +1,14 @@
 import { booksJsonResponse } from "../apihelpers/bookApi.js";
 import { bookModel } from "../models/bookModel.js";
+import {currentTitle} from "../models/helperModel.js";
 
-export const updateOLID = async (req,res) => {
+export function checkOLID(id,updatedTitle) {
 
   try{
+
+    const title = updatedTitle;
+    const currentTitle = currentTitle(id);
+
 
   } catch(error) {
     console.error("Unable to compare the title and updatedTitle",error);
