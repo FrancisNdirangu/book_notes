fd334430107090111d02e53b2a8cbdea22139ca5
Francis Ndirangu <frankngugi@gmail.com>
2026-08-17 16:35:33 +0300

    added a if statement that will update the olid and book cover link if the title is updated

--- models/helperModel.js
+++ models/helperModel.js
@@ -9,7 +9,7 @@ export function currentTitle(id) {
   return title
 }
 
-export function updateBookCover(id,updatedOLID,book_cover_link) {
+export async function  updateBookCover(id,updatedOLID,book_cover_link) {
   const query = "UPDATE book_notes SET olid = $1, book_cover_link=$2  WHERE id=$3"
   const update = db.query(query,[updatedOLID,book_cover_link,id]);
   console.log(`OLID and book cover link have been updated for id: ${id}`);
--- services/bookService.js
+++ services/bookService.js
@@ -1,16 +1,30 @@
 import { booksJsonResponse } from "../apihelpers/bookApi.js";
 import { bookModel } from "../models/bookModel.js";
-import {currentTitle} from "../models/helperModel.js";
+import {currentTitle, updateBookCover} from "../models/helperModel.js";
+import { booksJsonResponse } from "../apihelpers/bookApi.js";
 
-export function checkOLID(id,updatedTitle) {
+export function async checkOLID(id,updatedTitle) {
 
   try{
 
     const title = updatedTitle;
     const currentTitle = currentTitle(id);
 
+    if (title.toLowerCase().trim() !== currentTitle.toLowerCase().trim()) {
+      // call the helper api to get the new olid and update the db with the new olid
+      const bookJSON = await booksJsonResponse(updatedTitle);
+      const newOLID = await bookJSON['cover_edition_key'];
+      const link_base = "https://covers.openlibrary.ord/b/olid/"
+      const endOfLink = "-M.jpg"
+
+      const new_cover_link = link_base+newOLID+endOfLink;
+
+      const update = updateBookCover(id,newOLID,new_cover_link);
+
+
+    }
 
   } catch(error) {
     console.error("Unable to compare the title and updatedTitle",error);
   }
-}
+};
