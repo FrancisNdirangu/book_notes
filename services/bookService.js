ac898bce5adebc2ff4e7c61a6f4d2ef28af5bd8a
Francis Ndirangu <frankngugi@gmail.com>
2026-08-17 15:21:30 +0300

    made the imports into the bookService file and wrote the boilerplate of the updateOLID service function

---
+++ services/bookService.js
@@ -0,0 +1,11 @@
+import { booksJsonResponse } from "../apihelpers/bookApi.js";
+import { bookModel } from "../models/bookModel.js";
+
+export const updateOLID = async (req,res) => {
+
+  try{
+
+  } catch(error) {
+    console.error("Unable to compare the title and updatedTitle",error);
+  }
+}
