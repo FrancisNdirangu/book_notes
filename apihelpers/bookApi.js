a667230c1b452d0db01a002d7e13a12391542604
Francis Ndirangu <frankngugi@gmail.com>
2026-08-31 18:07:00 +0300

    added bookSearchResults api helper that will print the number of book results for a book

--- apihelpers/bookApi.js
+++ apihelpers/bookApi.js
@@ -12,3 +12,11 @@ export const booksJsonResponse = async (book_name) => {
   await sleep(2000); // a delay so that the api doesnt get rate limited
   return response.data['docs'][0];
 };
+
+export const bookSearchResults = async (book_name) => {
+  const properly_formatted_book_name = encodeURIComponent(book_name.trim()).replace(/%20/g,"+");
+  console.log(properly_formatted_book_name);
+  const response = await axios.get(process.env.SEARCH_URL+properly_formatted_book_name);
+  const numSearchResults = response.data['numFound'];
+  console.log(numSearchResults);
+}
