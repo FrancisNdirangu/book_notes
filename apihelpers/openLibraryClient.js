e1bcb6027969e5e87d3c59aa9faff001fdc6efdc
Francis Ndirangu <frankngugi@gmail.com>
2026-09-13 16:25:20 +0300

    finished creating the searchBookWithCovers client user function that will return information like title, author, olid, year published and book_cover_url

--- apihelpers/openLibraryClient.js
+++ apihelpers/openLibraryClient.js
@@ -22,7 +22,7 @@ const searchBookWithCover = async (title) => {
     const response = openLibraryClient.get('/search.json',{
       params: {
         q: title.trim(),
-        fields: "title,author_name,cover_edition_key,first_publish_year",
+        fields: "title,author_name,cover_edition_key,first_publish_year,series_key,series_name,series_position",
         lang:"eng",
         limit:3
       }
@@ -30,5 +30,30 @@ const searchBookWithCover = async (title) => {
 
     const book = response.data.docs[0]; //using the first result only despite the limit giving us the top 3 results in english
 
+    if (!book) {
+      return null;
+    }
+
+
+    // the line below start by checking if the olid exists, if it does it creates the url using the structure 
+    const coverURL = book.cover_edition_key ? `https://covers.openlibrary.org/a/olid/${book.cover_edition_key}-M.jpg` : null;
+
+
+    return {
+      title:book.title,
+      author:book.author_name ? book.author_name[0] : "Unknown Author",
+      olid: book.cover_edition_key,
+      year_published: book.first_publish_year,
+      book_cover_url: coverURL, //null if there is no url
+    }
+
+    //construct the cover api url using the olid
+    //we are setting the default to be false so that we actually get a 404 error when no cover is found instead of the default white pixel
+
+
+
+  } catch (error){
+    console.error("Open Library API Error:",error.message);
+    throw error;
   }
 }
