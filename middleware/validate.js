381c3c173f74088b0bc731be2ec25bb1a32136fc
Francis Ndirangu <frankngugi@gmail.com>
2026-09-24 17:37:22 +0300

    added an arrow function in validate.js that will validate the user's inputs when adding a new record and then rerender the addNotes page while passing the validation errors and the formData into it

---
+++ middleware/validate.js
@@ -0,0 +1,20 @@
+import Joi from "joi"
+
+export const validateAddBookNotes = (schema) => {
+  return (req,res,next) => {
+    const {error,value} = schema.validate(req.body)
+
+    if (error) {
+      const fieldErrors = {};
+      error.details.forEach( (item) => {
+        const errorName = item.path[0];
+        if(!fieldErrors[errorName]) {
+          fieldErrors[errorname] = error.message;
+        }
+
+        res.status(400).render('../views/addNotes.ejs',{fieldErrors,
+        formData:req.body});
+      })
+    }
+  }
+}
