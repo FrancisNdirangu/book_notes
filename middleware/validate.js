1687d48fde1c0524a24deba44401b4d402b3c092
Francis Ndirangu <frankngugi@gmail.com>
2026-09-25 16:48:28 +0300

    fixed an error by rendering the validation error outside the forEach and also returning the validation error so that when there is an error caught by the middleware it stops handing the controller empty information. when there was a validation error we were doing two renders. the validation render that reloads the page and shows the error and the controller render

--- middleware/validate.js
+++ middleware/validate.js
@@ -12,11 +12,12 @@ export const validateAddBookNotes = (schema) => {
         if(!fieldErrors[errorName]) {
           fieldErrors[errorName] = error.message;
         }
+      });
 
-        res.status(400).render('../views/addNotes.ejs',{fieldErrors,
+        return res.status(400).render('../views/addNotes.ejs',{fieldErrors,
         formData:req.body});
-      })
-    }
+      }
+    
     //this next below might not be good since we dont want the controller getting 
     //no data after we raise the validation error
     //next() //tells the middleware to move on to the controller
