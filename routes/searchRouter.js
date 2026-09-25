a563d9e157e1f23cf0814d39315fd86c57b977ff
Francis Ndirangu <frankngugi@gmail.com>
2026-09-25 14:53:20 +0300

    installed npm joi date, extended basejoi so i could use it. i also added the joi validation to the routers

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -12,6 +12,8 @@ import {
 } from "../controllers/viewController.js";
 import { nullOlidRows,createBookCoverLinks } from "../middleware/dbCheck.js";
 import express from "express";
+import { bookSchema } from "../validators/bookSchema.js";
+import { validateAddBookNotes } from "../middleware/validate.js";
 
 const router = express.Router();
 
@@ -20,7 +22,7 @@ router.get("/",nullOlidRows ,createBookCoverLinks,listAllBooks);
 router.get("/:id/view", viewSpecificReview);
 
 router.get("/addBookNotes", addBookPage);
-router.post("/addBookNotes", addBookNotes);
+router.post("/addBookNotes",validateAddBookNotes(bookSchema), addBookNotes);
 
 router.get("/:id/edit", editBookPage);
 router.post("/books/:id/edit",editBookNotes);
--- validators/bookSchema.js
+++ validators/bookSchema.js
@@ -1,8 +1,11 @@
-import Joi from "joi";
+import BaseJoi from "joi";
+import { JoiDate } from "@joi/date";
+
+const Joi = BaseJoi.extend(JoiDate);
 
 export const bookSchema = Joi.object({
-  title:Joi.string().trim().min(1).max().required(),
-  review:Joi.string().trim().trim().allow('').max(3000).optional(),
+  title:Joi.string().trim().min(1).max(300).required(),
+  review:Joi.string().trim().allow('').max(3000).optional(),
   dateRead:Joi.date().format('YYYY-MM-DD').max('now').required(),
   rating:Joi.number().integer().min(1).max(5).required()
 });
